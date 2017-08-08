6c45eba022bd5446de1eb2954e5a03029fc6b597
Omar Ihab Hamed Handouk <o.i.handouk@gmail.com>
2017-08-08 19:41:41 +0200

    Update Flood Fill (Coloring).java

--- Graphs/Traversal/Flood Fill (Coloring).java
+++ Graphs/Traversal/Flood Fill (Coloring).java
@@ -8,9 +8,37 @@ public class Main{
     static ArrayList<int[]> queries = new ArrayList<>();
     static boolean[][] visited;
 
+    static int[] dx = {0,-1,1,1,0,1,-1,-1};
+    static int[] dy = {1,1,0,1,-1,-1,0,-1};
+
+    static int flood(int x, int y){
+
+        if (!isValid(x,y)) return 0;
+
+        visited[x][y] = true;
+
+        int c = 1;
+
+        for (int i = 0 ; i < 8 ; i++){
+            int a = x + dx[i], b = y + dy[i];
+
+            if (isValid(a, b) && !visited[a][b] && grid.get(a)[b] == 'W')
+                c += flood(a, b);
+        }
+
+        return c;
+    }
+
+    static boolean isValid(int x, int y){
+
+        return 0 <= x && x < grid.size() && 0 <= y && y < grid.get(x).length;
+    }
+
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args){
+
+        //********This is Just the Input********
 
         while (true){
 
@@ -42,32 +70,6 @@ public class Main{
 
         grid = new ArrayList<>();
         queries = new ArrayList<>();
-    }
-
-    static int[] dx = {0,-1,1,1,0,1,-1,-1};
-    static int[] dy = {1,1,0,1,-1,-1,0,-1};
-
-    static int flood(int x, int y){
-
-        if (!isValid(x,y)) return 0;
-
-        visited[x][y] = true;
-
-        int c = 1;
-
-        for (int i = 0 ; i < 8 ; i++){
-            int a = x + dx[i], b = y + dy[i];
-
-            if (isValid(a, b) && !visited[a][b] && grid.get(a)[b] == 'W')
-                c += flood(a, b);
-        }
-
-        return c;
-    }
-
-    static boolean isValid(int x, int y){
-
-        return 0 <= x && x < grid.size() && 0 <= y && y < grid.get(x).length;
     }
 }
 
